47c69aee8afdb49b16ca92045463bc10e0f5c07c
mevctrader <mevctrader@gmail.com>
2022-09-16 21:14:20 +0000

    updatetarea

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["nuestro", "el"];
+  let adj = ["gran", "grandioso"];
+  let sustant = ["corredor", "jugador"];
+  let extensions = ["com", "es", "net", "io", "com.ve"];
+
+  const collection = document.getElementsByClassName("dominio");
+  collection.innerHTML = GenerarDominios(pronoun, adj, sustant, extensions);
 };
+
+function GenerarDominios(pronoun, adj, sustant, extensions) {
+  var element = document.querySelector(".dominio");
+
+  for (let i = 0; i <= 8; i++) {
+    let pronunciacion = Math.floor(Math.random() * pronoun.length);
+    let adjetivo = Math.floor(Math.random() * adj.length);
+    let sustantivo = Math.floor(Math.random() * sustant.length);
+    let extension = Math.floor(Math.random() * extensions.length);
+    let domin =
+      pronoun[pronunciacion] +
+      "" +
+      adj[adjetivo] +
+      "" +
+      sustant[sustantivo] +
+      "." +
+      extensions[extension];
+
+    let datoParrafo = document.createElement("p");
+
+    datoParrafo.innerHTML = domin;
+
+    element.appendChild(datoParrafo);
+
+    //console.log(domin);
+  }
+}
